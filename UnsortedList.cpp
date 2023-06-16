a479da215efe902de2ff1789e34787ac2a7cfdcb
Pravat Kiran Timsina <ptimsina@horizon.csueastbay.edu>
2023-06-16 00:21:30 -0700

    Additem, Delete Item, contains and get-length working.

--- UnsortedList.cpp
+++ UnsortedList.cpp
@@ -1,1 +1,59 @@
 
+#include "UnsortedList.h"
+
+UnsortedList::UnsortedList() {
+    length=0;
+    currentPos = 0;
+}
+
+void UnsortedList::MakeEmpty() {
+    length=0;
+}
+
+bool UnsortedList::IsFull() const {
+    return (MAX_ITEMS==length);
+}
+
+int UnsortedList::GetLength() const {
+    return length;
+}
+
+bool UnsortedList::Contains(int someItem) {
+    //look at all values in array
+    for(int i = 0; i < length; i++){
+        if (arr[i]==someItem){
+            return true;
+        }
+    }
+    //since non of the values in the array returned true, We know someItem is not in array.
+    return false;
+}
+
+void UnsortedList::AddItem(int item) {
+   // if(length<MAX_ITEMS){
+   if(!IsFull()){
+        arr[length]=item;
+        ++length;
+    }
+}
+
+void UnsortedList::DeleteItem(int item) {
+    //Find item
+    for (int i = 0; i<length;i++){
+        //found the item
+        if (arr[i]==item){
+            arr[i]=arr[length-1];
+            length--;
+            return;
+        }
+    }
+}
+
+void UnsortedList::ResetIterator() {
+
+}
+
+int UnsortedList::GetNextItem() {
+    return 0;
+}
+
